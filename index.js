538c80f4a062c3fc908b177248e7679743a5672a
Aaron Fowlkes <anbfowlkes@gmail.com>
2022-06-11 21:47:20 -0400

    commit tree 35091ee8e691b9964d4f1287bcd378167ced4dc3

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(strArr, event){
+    let myArr = [];
+    for (let i = 0; i<strArr.length; i++){
+        myArr.push(`Thank you, ${strArr[i]}, for the wonderful ${event} gift!`);
+    }
+    return myArr;
+}
+
+function countDown(n) {
+    let i = n;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
